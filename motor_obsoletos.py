39697f8fce0b4dba3658692a6790aa378c00b7c0
marcosabernardo-lgtm <marcos.a.bernardo@gmail.com>
2026-03-02 12:49:00 -0300

    Implement main function for processing project uploads
    Add main function to process uploaded project file and return dataframes.

---
+++ motor_obsoletos.py
@@ -0,0 +1,9 @@
+def executar_motor(uploaded_file):
+    """
+    Função principal que vai processar o PROJETO_UPLOAD.zip
+    e retornar:
+        - dataframe final
+        - dataframe exportável
+    """
+
+    return None, None
