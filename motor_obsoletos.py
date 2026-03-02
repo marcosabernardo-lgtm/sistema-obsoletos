2e897b4bb6bb4010f6c4d11f5e758476a1ccac56
marcosabernardo-lgtm <marcos.a.bernardo@gmail.com>
2026-03-02 17:13:52 -0300

    Update motor_obsoletos.py

--- motor_obsoletos.py
+++ motor_obsoletos.py
@@ -201,10 +201,11 @@ def executar_motor(uploaded_file):
         # MERGE FINAL
         # =====================================================
 
-# 🔎 TESTE: somente Robotica
-df_final = df_final[
-    df_final["Empresa / Filial"].str.contains("Robotica")
-]
+        df_final = df_estoque.merge(
+            df_mov_cons,
+            on="ID_UNICO",
+            how="left"
+        )
 
         df_final = df_final.drop(columns=["Empresa", "Filial"])
 
