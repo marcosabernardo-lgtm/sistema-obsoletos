4be7642ae11ca824fb36de2fd0876cda9a1dbdc2
marcosabernardo-lgtm <marcos.a.bernardo@gmail.com>
2026-03-04 08:57:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 import streamlit as st
 from motor_obsoletos import executar_motor
+from base_historica import atualizar_base_historica
+
 st.set_page_config(page_title="Sistema de Obsoletos", layout="wide")
+
 st.title("📦 Sistema de Controle de Obsoletos")
 st.markdown("---")
+
 st.subheader("Upload do Arquivo")
+
 uploaded_file = st.file_uploader(
     "Selecione o arquivo PROJETO_UPLOAD.zip",
     type=["zip"]
 )
+
 if uploaded_file is not None:
+
     if st.button("🚀 Processar Arquivo"):
+
         st.info("Processando arquivo...")
+
         df_final, df_export = executar_motor(uploaded_file)
+
         if df_final is not None:
+
+            # SALVAR NO HISTÓRICO
+            atualizar_base_historica(df_final)
+
             st.success("Processamento concluído com sucesso!")
+
             st.dataframe(df_final)
+
             st.download_button(
                 label="📥 Baixar Excel Final",
                 data=df_export,
                 file_name="Base_Obsoletos_Final.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
+
         else:
+
             st.error("Erro no processamento.")
