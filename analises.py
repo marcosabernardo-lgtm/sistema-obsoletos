ec71c15e0f47cfd1657bf6c92b848010a2b7737e
marcosabernardo-lgtm <marcos.a.bernardo@gmail.com>
2026-03-04 10:53:01 -0300

    Update analises.py

--- analises.py
+++ analises.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+
+def evolucao_estoque(df_hist):
+
+    total = (
+        df_hist
+        .groupby("Data Fechamento")["Custo Total"]
+        .sum()
+        .reset_index(name="Estoque Total")
+    )
+
+    obsoleto = (
+        df_hist[df_hist["Status Estoque"] == "Obsoleto"]
+        .groupby("Data Fechamento")["Custo Total"]
+        .sum()
+        .reset_index(name="Estoque Obsoleto")
+    )
+
+    df = total.merge(
+        obsoleto,
+        on="Data Fechamento",
+        how="left"
+    )
+
+    df["Estoque Obsoleto"] = df["Estoque Obsoleto"].fillna(0)
+
+    df["% Obsoleto"] = (
+        df["Estoque Obsoleto"] /
+        df["Estoque Total"]
+    )
+
+    return df.sort_values("Data Fechamento")
