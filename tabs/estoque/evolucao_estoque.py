7e3dcbbbae0269617d65afcc6e5547cacdf6a4c0
marcosabernardo-lgtm <marcos.a.bernardo@gmail.com>
2026-03-12 00:02:41 +0000

    feat: adiciona aba Redução Estoque com zerados e parciais

--- tabs/estoque/evolucao_estoque.py
+++ tabs/estoque/evolucao_estoque.py
@@ -4,6 +4,7 @@ from tabs.estoque.graficos.grafico_evolucao import render as grafico_evolucao
 from tabs.estoque.graficos.grafico_empresa import render as grafico_empresa
 from tabs.estoque.graficos.grafico_conta import render as grafico_conta
 from tabs.estoque.graficos.grafico_top_produtos import render as grafico_top_produtos
+from tabs.estoque.graficos.grafico_reducao_estoque import render as grafico_reducao_estoque
 
 
 def render(df_hist, moeda_br, df_kpi=None, data_selecionada=None, valor_mom=None, valor_yoy=None):
@@ -33,4 +34,4 @@ def render(df_hist, moeda_br, df_kpi=None, data_selecionada=None, valor_mom=None
         grafico_top_produtos(df, moeda_br, data_selecionada)
 
     with tab5:
-        st.info("Gráfico ainda não implementado")
+        grafico_reducao_estoque(df, moeda_br, data_selecionada)
